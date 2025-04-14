3f03d293a4e76eeb75c2ad223054d280c2464317
yichenlou <1360289301@qq.com>
2025-04-14 14:56:48 +0800

    V2

--- Yichen_LOU_20614825.m
+++ Yichen_LOU_20614825.m
@@ -1,14 +1,20 @@
 % Yichen LOU
 % ssyyl67@nottingham.edu.cn
 
-
 %% PRELIMINARY TASK - ARDUINO AND GIT INSTALLATION [10 MARKS]
 
-% Insert answers here
+% Initialize Arduino
+%a = arduino('COM6', 'Uno'); % Replace 'COM6' with your port
+
+% Blink LED on pin D3
+for i = 1:10
+    writeDigitalPin(a, 'D6', 1); % Turn LED on
+    pause(0.5);                  % Wait 0.5 seconds
+    writeDigitalPin(a, 'D6', 0); % Turn LED off
+    pause(0.5);                  % Wait 0.5 seconds
+end
 
 %% TASK 1 - READ TEMPERATURE DATA, PLOT, AND WRITE TO A LOG FILE [20 MARKS]
-
-% Insert answers here
 
 %% TASK 2 - LED TEMPERATURE MONITORING DEVICE IMPLEMENTATION [25 MARKS]
 
